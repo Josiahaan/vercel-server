794ec43a88f8343f188575ab506b98e43f7a8a2f
Jonathan Sebastian <j.shn50@gmail.com>
2024-07-25 14:17:24 +0700

    test

--- models/index.js
+++ models/index.js
@@ -18,8 +18,8 @@ const db = {};
 // const sequelize = new Sequelize('postgres://default:B2LqHctjg7iz@ep-curly-violet-a1oeik5m.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require', {
 //   dialectModule: require('pg')
 // });
-const sequelize = new Sequelize(`postgres://default:B2LqHctjg7iz@ep-curly-violet-a1oeik5m-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=true`, {
-  dialect: require('pg'),
+const sequelize = new Sequelize(`postgres://default:B2LqHctjg7iz@ep-curly-violet-a1oeik5m-pooler.ap-southeast-1.aws.neon.tech:5432/ssp-api?sslmode=true`, {
+  dialect: 'postgres',
   protocol: 'postgres',
   logging: false,
   dialectOptions: {
