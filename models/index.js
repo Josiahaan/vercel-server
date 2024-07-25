02563ac5689e82771a7ec7fbb8346a01fbc7d9b7
Jonathan Sebastian <j.shn50@gmail.com>
2024-07-25 10:58:25 +0700

    config

--- models/index.js
+++ models/index.js
@@ -9,15 +9,15 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-// let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
-const sequelize = new Sequelize('postgres://admin:admin@localhost:5432/mydb', {
-  dialectModule: require('pg')
-});
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, {dialectModule: require('pg')}, config);
+}
+// const sequelize = new Sequelize('postgres://admin:admin@localhost:5432/mydb', {
+//   dialectModule: require('pg')
+// });
 
 fs
   .readdirSync(__dirname)
